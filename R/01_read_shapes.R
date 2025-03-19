f0cf98565f1e588222fb8f619bed688f84b90c48
contreras-wk <luis.contreras.cm@gmail.com>
2025-03-19 14:24:37 -0600

    fix: spTransform y CRS

--- R/01_read_shapes.R
+++ R/01_read_shapes.R
@@ -5,6 +5,11 @@ read_shape <- function(shape_dsn, shape_layer, shape_file) {
     layer = shape_layer,
     verbose=FALSE
   )
-  shape_data <- sp::spTransform(shape_data, sp:::CRS("+init=epsg:4326"))
+  #shape_data <- sp::spTransform(shape_data, sp:::CRS("+init=epsg:4326"))
+  #shape_data <- sp::spTransform(shape_data, sp::CRS("EPSG:4326"))
+  shape_data <- sp::spTransform(
+    shape_data,
+    sp::CRS("+proj=longlat +datum=WGS84 +no_defs")
+  )
   shape_data
 }
